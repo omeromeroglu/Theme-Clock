0ea3f75365b2c9ae5c7733ab35b5096a2b931f13
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-01-18 12:25:40 +0300

    fixed second overall spin

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const hourEl = document.querySelector(".hour")
 const minuteEl = document.querySelector(".minute")
 const secondEl = document.querySelector(".second")
 const timeEl = document.querySelector(".time")
-const dataEl = document.querySelector(".date")
+const dateEl = document.querySelector(".date")
 const toggle = document.querySelector(".toggle")
 
 const days = [
@@ -48,16 +48,23 @@ function setTime() {
     const time = new Date()
     const month = time.getMonth()
     const day = time.getDay()
+    const date = time.getDate()
     const hours = time.getHours()
     const minutes = time.getMinutes()
     const seconds = time.getSeconds()
     const hoursForClock = hours % 12
+    const ampm = hours >= 12 ? "PM" : "AM"
 
     
-
-    hourEl.style.transform = `translate(-50%,-100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)})`
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale((hoursForClock * 60) + minutes, 0, 720, 0, 360)}deg)`
     minuteEl.style.transform = `translate(-50%,-100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
     secondEl.style.transform = `translate(-50%,-100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+    secondEl.style.transition = `${seconds === 0 ? "none" : "all 0.5s ease-in"}`;
+
+
+    timeEl.innerHTML= `${hoursForClock < 10 ?`0${hoursForClock}` : hoursForClock }:${minutes < 10 ?`0 ${minutes}` : minutes } ${ampm}`
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class= "circle"> ${date}`
 }
     const scale = (number, inMin, inMax, outMin, outMax) => {
         return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
